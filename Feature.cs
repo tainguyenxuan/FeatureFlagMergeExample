79e684cfdd75b6d95704b2a449b24dda8e0ed76b
Nguyen Xuan Tai <tai.nguyenxuan@employmenthero.com>
2025-03-14 15:08:58 +0700

    Init

--- Feature.cs
+++ Feature.cs
@@ -1,1 +1,28 @@
-Hello World
+using System;
+using System.ComponentModel;
+using Payroll.Utility.Attributes;
+using Reinforced.Typings.Attributes;
+
+namespace Payroll.Models.DomainModels.CommonDb.FeatureFlags
+{
+	public enum Feature
+	{
+		 #region TeamA Features
+        // TeamA owns this region
+        NewUIDesign,
+        ImprovedReporting,
+        #endregion
+
+        #region TeamB Features
+        // TeamB owns this region
+        BetaAlgorithm,
+        CloudSync,
+        #endregion
+
+        #region TeamC Features
+        // TeamC owns this region
+        NewPaymentProcessor,
+        EnhancedSecurity,
+        #endregion
+	}
+}
